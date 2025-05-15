8efd507ca5c807b2de319bd50e0ad17d1711329b
parpatb9 <patelparth1960@gmail.com>
2025-05-15 14:10:46 +1200

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,258 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+  {
+    name: "My Resume",
+    link: "https://drive.google.com/file/d/1ZhDFyJ7Gl-13WhPzIIUYxttKWbSehDlC/view?usp=drive_link",
+  },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description:
+      "I constantly try to improve and all the technology I use is mention in my Resume below.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a personal project using NextJS",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://parthiphone15pro.netlify.app",
+  },
+  {
+    id: 2,
+    title: "Movie App",
+    des: "Simple project using React, Tailwind CSS, and TMDB API to display movies.",
+    img: "/p2.png",
+    iconLists: ["/react.svg", "/tail.svg", "/ts.svg", "/restapi.svg"],
+    link: "https://parthreactmovieapp.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Brainstorm",
+    des: "A visually stunning website just for showcasing frontend skills.",
+    img: "/p3.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://parth-brainwave.netlify.app",
+  },
+  {
+    id: 4,
+    title: "A Production Ready API",
+    des: "A backend API project using Node.js and Express, with MongoDB for data storage. Includes JWT authentication and subscription reminder via Email.",
+    img: "/p4.png",
+    iconLists: ["/express.svg", "/node.svg", "/mongodb.svg"],
+    link: "https://github.com/parpatb9/subscription-tracking-api",
+  },
+  {
+    id: 5,
+    title: "Cocktail finder",
+    des: "Cocktail finder app using React and TheCocktailDB API.",
+    iconLists: ["/react.svg", "/tail.svg", "/restapi.svg"],
+    img: "/cocktail.png",
+    link: "https://cocktailfindmorderate.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Color Shades Generator",
+    des: "Color shades generator app using React and CSS.",
+    iconLists: ["/react.svg", "/tail.svg", "/restapi.svg"],
+    img: "/color.png",
+    link: "https://colorshades-generator.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "Lorem ipsum Generator",
+    des: "lorem ipsum generator app using React and CSS.",
+    iconLists: ["/react.svg", "/tail.svg", "/restapi.svg"],
+    img: "/lorem.png",
+    link: "https://loremipsumgeneratorbasic.netlify.app/",
+  },
+  {
+    id: 8,
+    title: "Basic Menu",
+    des: "Basic menu app using React and CSS.",
+    iconLists: ["/react.svg", "/tail.svg", "/restapi.svg"],
+    img: "/basicmenu.png",
+    link: "https://basicmenu001.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Parth was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Parth's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Parth is the ideal partner.",
+    name: "Agasth Bhadja",
+    title: "Project Manager at Optrek",
+  },
+  {
+    quote:
+      "Working with Parth was a fantastic experience. His professionalism, efficiency, and commitment to delivering top-notch results were evident throughout our collaboration. His passion for every aspect of development truly sets him apart. If you're looking to enhance your website and strengthen your brand, Parth is the perfect choice.",
+    name: "Harpreet Singh",
+    title: "Colleague at Optrek",
+  },
+  {
+    quote:
+      "Collaborating with Parth was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Parth's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Parth is the ideal partner.",
+    name: "Agasth Bhadja",
+    title: "Project Manager at Optrek",
+  },
+  {
+    quote:
+      "Working with Parth was a fantastic experience. His professionalism, efficiency, and commitment to delivering top-notch results were evident throughout our collaboration. His passion for every aspect of development truly sets him apart. If you're looking to enhance your website and strengthen your brand, Parth is the perfect choice.",
+    name: "Harpreet Singh",
+    title: "Colleague at Optrek",
+  },
+  {
+    quote:
+      "Collaborating with Parth was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Parth's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Parth is the ideal partner.",
+    name: "Agasth Bhadja",
+    title: "Project Manager at Optrek",
+  },
+  {
+    quote:
+      "Working with Parth was a fantastic experience. His professionalism, efficiency, and commitment to delivering top-notch results were evident throughout our collaboration. His passion for every aspect of development truly sets him apart. If you're looking to enhance your website and strengthen your brand, Parth is the perfect choice.",
+    name: "Harpreet Singh",
+    title: "Colleague at Optrek",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern - Silverwing",
+    desc: "Assisted senior developers in building web pages using HTML5, CSS, and JavaScript, gaining hands-on experience in front-end development.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Developer - DigiIO",
+    desc: "Developed responsive web components with ReactJS, collaborating with designers.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Web Developer - Optrek",
+    desc: "Lead front-end development of web applications using ReactJS, creating reusable components and ensuring responsive design.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/parpatb9",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/p_patel01/",
+  },
+];
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://366ad628cac93afb4df72194789337e0@o4509228512182272.ingest.us.sentry.io/4509315383689216",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
